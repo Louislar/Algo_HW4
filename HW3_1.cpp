fef5bf184d3c9c5070fdaa0c7acbc7f33caeb68f
Louislar <a8945267@gmail.com>
2018-04-16 21:34:45 +0800

    Add files via upload

--- HW3_1.cpp
+++ HW3_1.cpp
@@ -28,15 +28,18 @@ double distances(dot p, dot q) //test pass
     return sum;
 }
 
-int closest(dot points[], int pointsLength)
+double closest(dot points[], int pointsLength)
 {
     if(pointsLength==2)
     {
         return distances(points[0], points[1]);
     }
+    else if(pointsLength==1) return 10000;
+    else{
 
-    /*******************start form here*************************/
+
     int medianIndex=pointsLength/2; //find the median
+    int median=points[medianIndex].x;
 
     dot sL[medianIndex+1];
     dot sR[pointsLength-(medianIndex+1)];
@@ -51,16 +54,65 @@ int closest(dot points[], int pointsLength)
         sR[e]=points[w];
     }
 
-    int dL=closest(sL, medianIndex+1);
-    int dR=closest(sR, pointsLength-(medianIndex+1));
-    int d=10000;
+
+    double dL=closest(sL, medianIndex+1);
+    cout<<"dL: "<<dL<<endl;
+    double dR=closest(sR, pointsLength-(medianIndex+1));
+    cout<<"dR: "<<dR<<endl;
+    double d=10000;
+
 
     //min(dL, dR)
     if(dL>dR) d=dR;
     else if(dR>dL) d=dL;
-    else {d=dR;}
+    else if(dR==dL) d=dR;
+
+    cout<<d<<endl;
+
+    //the points which's x is between m-d and m
+    int point_pLength=0;
+    dot point_p[pointsLength];
+    for(int w=0;w<pointsLength;w++)
+    {
+        if(points[w].x<=median&&points[w].x>=median-d)
+        {
+            point_p[point_pLength]=points[w];
+            point_pLength++;
+        }
+    }
+
+    //the points which's x is between m and m+d
+    int point_qLength=0;
+    dot point_q[pointsLength];
+    for(int w=0;w<pointsLength;w++)
+    {
+        if(points[w].x<=median+d&&points[w].x>median)
+        {
+            point_q[point_qLength]=points[w];
+            point_qLength++;
+        }
+    }
+
+    // points which's x is between m and m+d,
+    // also its y is between yP-d and yP+d,
+    // then calculate those point's distance with p
+    for(int w=0;w<point_pLength;w++)
+    {
+        double yP=point_p[w].y;
+        for(int e=0;e<point_qLength;e++)
+        {
+            if(point_q[e].y<=yP+d&&point_q[e].y>=yP-d)
+            {
+                double d_=distances(point_p[w], point_q[e]);
+                if(d_<d) d=d_;
+            }
+        }
+    }
+
+    return d;
 
 
+    }//else end
 }
 
 //test
@@ -71,7 +123,7 @@ int main()
 
     dot ps[5];
     for(int w=0;w<5;w++){ps[w]=dot(w, w);}
-    closest(ps, 5);
+    cout<<closest(ps, 5);
 }
 
 
