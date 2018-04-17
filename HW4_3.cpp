811312dbaa8db108a32dfe5e00a228490091a82a
Louislar <a8945267@gmail.com>
2018-04-17 11:27:35 +0800

    Add files via upload

--- HW4_3.cpp
+++ HW4_3.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<queue>
+#include<math.h>
 
 using namespace std;
 
@@ -70,43 +71,72 @@ int MergeSort(int a[], int s, int e)
     MergeSort(a, q+1, e);
     for(int i=q+1;i<=e;i++) myqueue.push(a[i]);
 
+
+
     Merge(a, s, q, e);
     if(e==tIndex){
         int temp1=e-s+1;
         int temp2=tIndex+1;
-        int count1=0;
-        while(temp2!=temp1) {temp2=temp2/2; count1++;}
+        temp2=temp2/temp1;
+
         for(int i=0;i<=tIndex;i++) cout<<a[i]<<" ";
         cout<<endl;
     }
     return 0;
 }
 
-int main()
+//test
+int main01()
 {
     int a[8]={3, 5, 2, 4, 1, 0, 7, 6};
     tIndex=7;
     MergeSort(a, 0, 7);
     for(int i=0;i<8;i++) cout<<a[i]<<" ";
+    cout<<"\nB: \n";;
+
+    int b[8]={3, 5, 2, 4, 1, 0, 7, 6};
+    int k=3;
+    int l=8;
+    for(int e=1;e<=k;e++)
+    {
+        int temp1=pow(2, e);
+        for(int i=0;i<l;i=i+temp1)
+        {
+            Merge(b, i, (2*i+temp1-1)/2, i+temp1-1);
+        }
+
+        //print B
+        for(int i=0;i<l;i++) cout<<b[i]<<" ";
+        cout<<endl;
+    }
+
 }
 
-int main01()
+int main()
 {
     int k=0;
     cin>>k;
-    int l=1;
-    while(k!=0)
+    int l=pow(2, k);
+
+    // 2^k=l
+
+    int nums[l]={0};
+    for(int i=0;i<l;i++) cin>>nums[i];
+
+    tIndex=l-1;
+
+    for(int e=1;e<=k;e++)
     {
-        l=l*2;
-        k--;
+        int temp1=pow(2, e);
+        for(int i=0;i<l;i=i+temp1)
+        {
+            Merge(nums, i, (2*i+temp1-1)/2, i+temp1-1);
+        }
+
+        //print B
+        for(int i=0;i<l;i++) cout<<nums[i]<<" ";
+        cout<<endl;
     }
-    k=l;
-
-    int nums[k]={0};
-    for(int i=0;i<k;i++) cin>>nums[i];
-
-    tIndex=k-1;
-    MergeSort(nums, 0, k-1);
 
 
 
